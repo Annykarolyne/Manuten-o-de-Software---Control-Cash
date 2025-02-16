7205e6bcc9f3ca35859db58da44467efdbc42816
larasous <sousalara822@gmail.com>
2025-02-16 19:33:20 -0300

    :recycle: refactor: dividindo métodos de atualização de fornecedor para terem responsabilidade única

--- fornecedor/urls.py
+++ fornecedor/urls.py
@@ -3,7 +3,8 @@ from .views import (
     listar_fornecedores, 
     cadastrar_fornecedor_get,
     cadastrar_fornecedor_post,
-    atualizar_fornecedor, 
+    atualizar_fornecedor_get, 
+    atualizar_fornecedor_post,
     vizualizar_fornecedor, 
     deletar_fornecedor
 )
@@ -12,7 +13,8 @@ urlpatterns = [
     path('listar/', listar_fornecedores, name='fornecedor_listar'),
     path('cadastrar/', cadastrar_fornecedor_get, name='fornecedor_cadastrar'),
     path('cadastrar/post/', cadastrar_fornecedor_post, name='cadastrar_fornecedor_post'),
-    path('atualizar/<int:id>/', atualizar_fornecedor, name='fornecedor_atualizar'),
+    path('atualizar/<int:id>/', atualizar_fornecedor_get, name='fornecedor_atualizar'),
+    path('atualizar/<int:id>/post/', atualizar_fornecedor_post, name='atualizar_fornecedor_post'),
     path('vizualizar/<int:id>/', vizualizar_fornecedor, name='fornecedor_vizualizar'),
     path('deletar/<int:id>/', deletar_fornecedor, name='fornecedor_deletar'),
 ]
