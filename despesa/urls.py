a6d528035fea62209c76efca157334517a35d9db
Marcelo Albuquerque <marcelo03@alu.ufc.br>
2025-02-16 21:13:04 -0300

    :recycle: refactor: dividindo responsabilidade do método criar despesa

--- despesa/urls.py
+++ despesa/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path
-
-from despesa.views import (criar_despesa_view, editar_despesa_view,
-                           listar_despesas_view, remover_despesa_view)
+from despesa.views import (
+    listar_despesas_view,
+    criar_despesa_get_view,
+    criar_despesa_post_view,
+    editar_despesa_view,
+    remover_despesa_view
+)
 
 app_name = 'despesa'
 
 urlpatterns = [
     path('', listar_despesas_view, name='despesa_listar'),
-    path('criar', criar_despesa_view, name='despesa_criar'),
-    path('editar/<int:pk>', editar_despesa_view, name='despesa_editar'),
-    path('remover/<int:pk>', remover_despesa_view, name='despesa_remover'),
+    path('criar/', criar_despesa_get_view, name='despesa_criar_get'),
+    path('criar/post/', criar_despesa_post_view, name='despesa_criar_post'),
+    path('<int:pk>/editar/', editar_despesa_view, name='despesa_editar'),
+    path('<int:pk>/remover/', remover_despesa_view, name='despesa_remover'),
 ]
